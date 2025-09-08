cd317c96f94e97bdd511f64e26a68a91b7833ac5
Aether <kunwarvinayak2005@gmail.com>
2025-09-08 17:52:59 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -35,6 +36,7 @@ const App = () => {
           </BrowserRouter>
         )}
         <SpeedInsights />
+        <Analytics/>
       </TooltipProvider>
     </QueryClientProvider>
     
